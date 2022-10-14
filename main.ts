aff097cfb9107a6a2bcb9cf3cd402e2a083f7625
Bioneisme <bioneisme@gmail.com>
2022-10-14 18:25:05 +0600

    Updated

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
-import {mainMenu} from "./menu/main-menu";
+import {mainMenu} from "./views/main-menu";
 import * as mongoose from "mongoose";
+import {UserClass} from "./controllers/user-controller";
 
 const start = async () => {
     await mongoose.connect('mongodb://localhost:27017/test', {});
+    UserClass.getInstance();
 
     mainMenu();
 }
