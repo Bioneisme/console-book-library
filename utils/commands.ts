c4af4ea2af2acebd2c22c965490cc0ab13d58435
Bioneisme <bioneisme@gmail.com>
2022-10-31 10:15:19 +0600

    Updated help command

---
+++ utils/commands.ts
@@ -0,0 +1,8 @@
+export const help = 'You can control menu using the arrows, or by pressing the corresponding keyboard keys\n' +
+    '* Profile - user\'s personal account\n' +
+    'Here you can view your personal information, create new books or log out\n' +
+    'If you are not logged in, you will be asked to log in or register\n' +
+    '* All Books - get a list of all books\n' +
+    '* Get a Book - get information about a book by title\n' +
+    '* Add a new Book\n' +
+    '* Exit - close the application (or Ctrl + C)';
